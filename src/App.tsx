c3a3cde21f13fedabd4221a20421190ff86c0380
Ashish Malla <ashishmalla45@gmail.com>
2022-10-26 21:29:42 -0600

    object props type

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Greet from './components/Greet'
+import Person from './components/Person'
 
 const App = () => {
+	const personName = {
+		first: 'Asiisii',
+		last: 'M',
+	}
 	return (
 		<div className='app'>
-			<Greet 
-        name='Asiisii'
-      />
+			<Greet name='Asiisii' messageCount={10} isLoggedIn={false} />
+			<Person name={personName} />
 		</div>
 	)
 }
